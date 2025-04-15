e64f30b74118ece886660bb5fa7ed725ac799302
prioritize <jared@prototypic.al>
2025-04-14 17:31:46 -0700

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
 #![no_std]
 #![no_main]
-use core::cell::RefCell;
-use cortex_m::asm::nop;
-use defmt::{assert_eq, println, unwrap};
+use defmt::{assert_eq, println};
 use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
 use embassy_executor::Spawner;
-use embassy_stm32::i2c::{self, Error, I2c};
+use embassy_stm32::i2c::{self, I2c};
 use embassy_stm32::mode::Async;
 use embassy_stm32::time::Hertz;
 use embassy_stm32::{bind_interrupts, peripherals};
-use embassy_sync::blocking_mutex::raw::{
-    CriticalSectionRawMutex, NoopRawMutex, ThreadModeRawMutex,
-};
+use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, ThreadModeRawMutex};
 use embassy_sync::channel::Channel;
 use embassy_sync::mutex::Mutex;
 use embassy_sync::signal::Signal;
 use embassy_time::{Duration, Timer};
 use embedded_hal_async::i2c::I2c as async_i2c;
+use fxas2100::FXAS2100;
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -26,12 +23,13 @@ const WHOAMI: u8 = 0x0C;
 const CTRL_REG1: u8 = 0x13;
 const EXPECTED_NAME: u8 = 0xD7;
 
-static I2C1_BUS: StaticCell<Mutex<CriticalSectionRawMutex, I2c<'static, Async>>> =
+static I2C2_BUS: StaticCell<Mutex<CriticalSectionRawMutex, I2c<'static, Async>>> =
     StaticCell::new();
+
 bind_interrupts!(
     struct Irqs {
-    I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
-    I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
+    I2C2_EV => i2c::EventInterruptHandler<peripherals::I2C2>;
+    I2C2_ER => i2c::ErrorInterruptHandler<peripherals::I2C2>;
 });
 
 static ODR_SHARED: Mutex<ThreadModeRawMutex, u32> = Mutex::new(0);
@@ -40,6 +38,9 @@ static GYRO_DATA: Mutex<ThreadModeRawMutex, [u8; 6]> = Mutex::new([0u8; 6]);
 static GYRO_COLLECT_SC: StaticCell<Signal<CriticalSectionRawMutex, bool>> = StaticCell::new();
 static GYRO_DATA_CHANNEL: StaticCell<Channel<CriticalSectionRawMutex, [u8; 6], 10>> =
     StaticCell::new();
+static FXAS_CELL: StaticCell<
+    FXAS2100<I2cDevice<'static, CriticalSectionRawMutex, I2c<'static, Async>>>,
+> = StaticCell::new();
 
 #[embassy_executor::task]
 async fn read_i2c_whoami(
@@ -78,8 +79,18 @@ async fn read_ctrl_reg1(mut i2c: I2cDevice<'static, CriticalSectionRawMutex, I2c
 }
 
 #[embassy_executor::task]
+async fn gyro_handler(
+    fxas: &'static fxas2100::FXAS2100<
+        I2cDevice<'static, CriticalSectionRawMutex, I2c<'static, Async>>,
+    >,
+) {
+    fxas.state_handler();
+}
+#[embassy_executor::task]
 async fn gyro_worker(
-    fxas: fxas2100::FXAS2100<I2cDevice<'static, CriticalSectionRawMutex, I2c<'static, Async>>>,
+    fxas: &'static fxas2100::FXAS2100<
+        I2cDevice<'static, CriticalSectionRawMutex, I2c<'static, Async>>,
+    >,
     mut i2c: I2cDevice<'static, CriticalSectionRawMutex, I2c<'static, Async>>,
     gyro_channel: &'static Channel<CriticalSectionRawMutex, [u8; 6], 10>,
 ) {
@@ -132,12 +143,12 @@ async fn main(spawner: Spawner) {
     let mut z_gyro_data = 0u16;
 
     let i2c = I2c::new(
-        p.I2C1,
-        p.PB8,
-        p.PB9,
+        p.I2C2,
+        p.PF1,
+        p.PF0,
         Irqs,
-        p.GPDMA1_CH0,
-        p.GPDMA1_CH1,
+        p.DMA1_CH7,
+        p.DMA1_CH2,
         Hertz(100_000),
         Default::default(),
     );
@@ -147,15 +158,16 @@ async fn main(spawner: Spawner) {
         GYRO_DATA_CHANNEL.init(Channel::new());
     // let _ = i2c.write_read(FXAS2100_ADDRESS, &[CTRL_REG1], &mut odr_data);
     let i2c_bus = Mutex::new(i2c);
-    let i2c_bus = I2C1_BUS.init(i2c_bus);
+    let i2c_bus = I2C2_BUS.init(i2c_bus);
     let i2c2_device1 = I2cDevice::new(i2c_bus);
     let i2c2_device2 = I2cDevice::new(i2c_bus);
     let i2c2_device3 = I2cDevice::new(i2c_bus);
     let i2c2_device4 = I2cDevice::new(i2c_bus);
 
-    let mut fxas = fxas2100::FXAS2100::new(i2c2_device4, FXAS2100_ADDRESS, gyro_signal);
+    let fxas_interior = fxas2100::FXAS2100::new(i2c2_device4, FXAS2100_ADDRESS, gyro_signal);
+    let mut fxas = FXAS_CELL.init(fxas_interior);
     let who_am_i = fxas.read_register(fxas2100::registers::WHO_AM_I).await;
-    let current_state = fxas.set_active().await;
+    //let current_state = fxas.set_active().await;
     assert_eq!(0xD7, who_am_i);
     println!("Made it past the assert");
     let _ = spawner.spawn(read_i2c_whoami(i2c2_device1));
@@ -166,20 +178,20 @@ async fn main(spawner: Spawner) {
     loop {
         Timer::after_millis(50).await;
         if value == 0 {
-            gyro_signal.signal(true);
+            fxas.signal_inactive();
             value = 1;
         }
         while gyro_channel.len() != 0 {
             println!("{}", gyro_channel.len());
-           match &gyro_channel.try_receive() {
-               Ok(data) => {
-                   x_gyro_data = ((data[0] as u16) << 8 ) | (data[1] as u16);
-                   y_gyro_data = ((data[2] as u16) << 8 ) | (data[3] as u16);
-                   z_gyro_data = ((data[4] as u16) << 8 ) | (data[5] as u16);
-                   println!("{}, {}, {}", x_gyro_data, y_gyro_data, z_gyro_data);
-               },
-               Err(_) => {}
-           }
+            match &gyro_channel.try_receive() {
+                Ok(data) => {
+                    x_gyro_data = ((data[0] as u16) << 8) | (data[1] as u16);
+                    y_gyro_data = ((data[2] as u16) << 8) | (data[3] as u16);
+                    z_gyro_data = ((data[4] as u16) << 8) | (data[5] as u16);
+                    println!("{}, {}, {}", x_gyro_data, y_gyro_data, z_gyro_data);
+                }
+                Err(_) => {}
+            }
         }
     }
 }
